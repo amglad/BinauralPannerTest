6aeb36434396b1348cd70909135521b5a05149d4
amglad <amglad@icloud.com>
2023-05-02 10:49:20 -0500

    almost working

--- BinauralPannerTest/Source/InterpolationDSP.cpp
+++ BinauralPannerTest/Source/InterpolationDSP.cpp
@@ -14,12 +14,12 @@ InterpolationDSP::InterpolationDSP() : fft (fftOrder)
 {
     
     // Reading SOFA file
-    bool success = sofa.readSOFAFile("/Users/mitchglad/BinauralPannerTest/BinauralPannerTest/Source/SOFA/SmallTheaterHRIRsPart2_1.0.sofa");
-    bool success2;
-    if(!success)
-        success2 = sofa.readSOFAFile("/Users/erictarr/BinauralPannerTest/BinauralPannerTest/Source/SOFA/SmallTheaterHRIRsPart2_1.0.sofa");
-    if(!success2)
-        return;
+//    bool success = sofa.readSOFAFile("/Users/mitchglad/BinauralPannerTest/BinauralPannerTest/Source/SOFA/SmallTheaterHRIRsPart2_1.0.sofa");
+//    bool success2;
+//    if(!success)
+//        success2 = sofa.readSOFAFile("/Users/erictarr/BinauralPannerTest/BinauralPannerTest/Source/SOFA/SmallTheaterHRIRsPart2_1.0.sofa");
+//    if(!success2)
+//        return;
 
 }
 
@@ -27,31 +27,35 @@ InterpolationDSP::InterpolationDSP() : fft (fftOrder)
 void InterpolationDSP::getHRIR(float az, float el, float d, juce::AudioBuffer<float> & buffer)
 {
    // int dI = static_cast <int> (d);
+    if (az < 0) {
+        az = az + 360;
+    }
+    if (el < 0) {
+        el = el + 360;
+    }
     
     if(d == 2.f || d == 6.f || d == 10.f || d == 14.f) // If in HRIR database
     {
         for (int c = 0; c < buffer.getNumChannels() ; c++){
             // Getting HRIRs from .sofa file
-//            const double *hrir = sofa.getHRIR(c, az, el, d);
+            //            const double *hrir = sofa.getHRIR(c, az, el, d);
             
+            int azInt = static_cast <int> (az);
+            int elInt = static_cast <int> (el);
+            int dInt = static_cast <int> (d);
             
-            
-            
-            
-            
-            // Turning parameters into strings
-            std::string azSt = std::to_string(static_cast <int> (az));
-            std::string elSt = std::to_string(static_cast <int> (el));
-            std::string dSt = std::to_string(static_cast <int> (d));
+            std::string azSt = std::to_string(azInt);
+            std::string elSt = std::to_string(elInt);
+            std::string dSt = std::to_string(dInt);
             
             // Writes string
             if (c == 0)
             {
-                name = "_az" + azSt + "_el" + elSt + "_d" + dSt + "_L_wav";
+                name = "az" + azSt + "_el" + elSt + "_d" + dSt + "_L_wav";
             }
             else
             {
-                name = "_az" + azSt + "_el" + elSt + "_d" + dSt + "_R_wav";
+                name = "az" + azSt + "_el" + elSt + "_d" + dSt + "_R_wav";
             }
         
             // Getting to the right format
@@ -59,14 +63,10 @@ void InterpolationDSP::getHRIR(float az, float el, float d, juce::AudioBuffer<fl
             
 //            const auto name = "_96k_Test_wav"; //BinaryData::getNamedResourceOriginalFilename(BinaryData::namedResourceList[0]);
             // Setting size and reading binary data
-            int irDataSize = 8236;
+            int irDataSize = 4140;
             auto* irData = BinaryData::getNamedResource(name1,irDataSize);
             
-            
-            
-            
-            
-            
+
             // Writing this data to a float array
             for(int n = 0; n < hrirSize; ++n)
             {
@@ -93,9 +93,7 @@ void InterpolationDSP::getHRIR(float az, float el, float d, juce::AudioBuffer<fl
 //            auto *hrirLow = sofa.getHRIR(c, az, el, dLow);
 //            auto *hrirHigh = sofa.getHRIR(c, az, el, dHigh);
             
-            
-            
-            
+        
             
             // Turning parameters into strings
             std::string azSt = std::to_string(static_cast <int> (az));
@@ -106,13 +104,13 @@ void InterpolationDSP::getHRIR(float az, float el, float d, juce::AudioBuffer<fl
             // Writes string
             if (c == 0)
             {
-                nameLow = "_az" + azSt + "_el" + elSt + "_d" + dLowSt + "_L_wav";
-                nameHigh = "_az" + azSt + "_el" + elSt + "_d" + dHighSt + "_L_wav";
+                nameLow = "az" + azSt + "_el" + elSt + "_d" + dLowSt + "_L_wav";
+                nameHigh = "az" + azSt + "_el" + elSt + "_d" + dHighSt + "_L_wav";
             }
             else
             {
-                nameLow = "_az" + azSt + "_el" + elSt + "_d" + dLowSt + "_R_wav";
-                nameHigh = "_az" + azSt + "_el" + elSt + "_d" + dHighSt + "_L_wav";
+                nameLow = "az" + azSt + "_el" + elSt + "_d" + dLowSt + "_R_wav";
+                nameHigh = "az" + azSt + "_el" + elSt + "_d" + dHighSt + "_L_wav";
             }
             
             // Getting to the right format
@@ -121,7 +119,7 @@ void InterpolationDSP::getHRIR(float az, float el, float d, juce::AudioBuffer<fl
             
 //            const auto name = "_96k_Test_wav"; //BinaryData::getNamedResourceOriginalFilename(BinaryData::namedResourceList[0]);
             // Setting size and reading binary data
-            int irDataSize = 8236;
+            int irDataSize = 4140;
             auto* irDataLow = BinaryData::getNamedResource(nameLow1,irDataSize);
             auto* irDataHigh = BinaryData::getNamedResource(nameHigh1,irDataSize);
             
